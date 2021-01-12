2ac799c73be342485f2ca980b35252b1566639b2
anasigbaria <44587049+anasigbaria@users.noreply.github.com>
2021-01-12 13:08:42 +0200

    Add files via upload

---
+++ terraform/variables.tf
@@ -0,0 +1,8 @@
+variable "accessKey" {
+  type        = string
+  description = "the access key of your aws user."
+}
+variable "secretKey" {
+  type        = string
+  description = "the secret key of your aws user"
+}
